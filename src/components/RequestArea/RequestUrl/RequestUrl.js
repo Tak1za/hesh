cd22dd918322b87afd1f4ffb91b3f4fe38b311ce
varun <varungupta2015135@gmail.com>
2020-08-02 14:36:17 +0530

    Added button to send request and positioned it properly

--- src/components/RequestArea/RequestUrl/RequestUrl.js
+++ src/components/RequestArea/RequestUrl/RequestUrl.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { Form, Col, Row } from "react-bootstrap";
+import { Form, Col, Row, Button } from "react-bootstrap";
 import "./RequestUrl.scss";
 
 const RequestUrl = () => {
   return (
     <Form.Group as={Row}>
-      <Col sm="12">
+      <Col sm="9">
         <Form.Control
           type="text"
           placeholder="Enter URL here..."
           className="request-url"
         />
+      </Col>
+      <Col sm="3">
+        <Button
+          variant="outline-primary"
+          type="submit"
+          className="request-button"
+        >
+          Send
+        </Button>
       </Col>
     </Form.Group>
   );
